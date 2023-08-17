f891816d52c0ee8c36876ef127659fad6ab21064
Kelvin Onumajuru <kelvinonumajuru@gmail.com>
2023-08-17 11:30:03 +0100

    Update 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -4,10 +4,11 @@ export default function printBestStudents(grades) {
   const bestStudent = Seq(grades)
     .filter((student) => student.score >= 70)
     .map((student) => ({
-      score: x.score,
+      ...student,
       firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
       lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
     }));
 
-  console.log(bestStudent.toObject());
+  console.log(bestStudent.toJS());
 }
+
